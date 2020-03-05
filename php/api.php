060c826d9ddf9b6de6b364af12639ad56eb1467f
Seilo <meetthedealer@gmail.com>
2020-03-05 06:31:37 +0100

    Unterscheidung nach Request Type hinzugefügt
    Abarbeitung POST in eigene Methode

--- php/api.php
+++ php/api.php
@@ -1,118 +1,143 @@
-<!doctype html>
-<html>
-	<head>
-		<title>TikiSpeiseplan API</title>
-	</head>
+<?php
+
+	/*	Erwartetes JSON Format
+	{
+		"password" : "****",
+		"operation": "add",
+		"restaurant": "Kunzmann",
+		"day": "2020-03-02",
+		"description" : "Leggumes",
+		"additional_description" : "mit Soße",
+		"price" : "4.20€"
+	}
+	*/
+
+	// Klassendefinitionen hinzufügen
+	require_once('classes.php');
+	require_once('etc.php');
+
+	// GET or POST erlaubt
+	$request_type = $_SERVER['REQUEST_METHOD'];
+
+	if($request_type == 'GET') {
+		get();
+	}		
+	else if ($request_type == 'POST') {
+		post();
+	}
+	else {
+		echo 'Invalid request type, use GET or POST';
+		return;
+	}
 	
-	<body>
-		<?php
+	/* ---------------------------------------------------------------
+	// Bearbeitung POST Request
+	----------------------------------------------------------------*/
+	function post() {
+		$json = file_get_contents('php://input');
 
-			/*	Erwartetes JSON Format
-			{
-				"password" : "****",
-				"operation": "add",
-				"restaurant": "Kunzmann",
-				"day": "2020-03-02",
-				"description" : "Leggumes",
-				"additional_description" : "mit Soße",
-				"price" : "4.20€"
-			}
-			*/
+		// Prüfe ob überhaupt Daten übergeben wurden
+		if(!isset($json)) {
+			echo 'No data in POST request.';
+			return;
+		}
 
-			// Klassendefinitionen hinzufügen
-			require_once('classes.php');
-			require_once('etc.php');
+		$obj = json_decode($json);
 
-			// Daten von Request entgegennehmen
-			$json = file_get_contents('php://input');
+		// Prüfe übergebene Daten
+		if(!validateData($obj)) {
+			echo 'Invalid data in POST request.';
+			return;
+		}
 
-			// Prüfe ob überhaupt Daten übergeben wurden
-			if(!isset($json)) errorMessage();
+		// MySQL Verbindung herstellen
+		$pdo = new PDO('mysql:host=localhost;dbname=food', 'test', $obj->password);
 
-			// Input Daten interpretieren
-			$obj = json_decode($json);
+		// Hole zulässige Retaurants ab
+		$restaurants = getRestaurantList($pdo);
+		$restaurant = getRestaurantByName($obj->restaurant, $restaurants);
+		if(!isset($restaurant)) {
+			echo 'Restaurant unknonwn.';
+			return;
+		}
 
-			// Prüfe übergebene Daten
-			if(!validateData($obj)) {
-				errorMessage();
-				return;
-			}
+		// Erstelle MenuItem Objekt
+		$menu_item = createMenuItem($restaurant, $obj);
 
-			// MySQL Verbindung herstellen
-			$pdo = new PDO('mysql:host=localhost;dbname=food', 'test', $obj->password);
+		// Sende die Daten an die Datenbank
+		if($obj->operation == "add") {
+			sendToDatabase($pdo, $menu_item);
+		}
+		else {
+			echo "Unknown Request";
+			return;
+		}
+	}
 
-			// Hole zulässige Retaurants ab
-			$restaurants = getRestaurantList($pdo);
-			$restaurant = getRestaurantByName($obj->restaurant, $restaurants);
-			if(!isset($restaurant)) {
-				errorMessage();
-				return;
-			}
+	/* ---------------------------------------------------------------
+	// Prüft die übergebene Daten, bevor diese in die Datenbank 
+	// geschrieben werden.
+	// --------------------------------------------------------------*/
+	function validateData($obj) {
+		$dataValid = true;
+		$allowed_operations = array('add');
 
-			// Erstelle MenuItem Objekt
-			$menu_item = createMenuItem($restaurant, $obj);
+		// Prüfe ob alle notwendigen Daten vorhanden sind
+		if(!isset($obj->operation)) $dataValid = false;
+		if(!in_array($obj->operation, $allowed_operations)) $dataValid = false;
+		if(!isset($obj->restaurant)) $dataValid = false;
+		if(!isset($obj->day)) $dataValid = false;
+		if(!isset($obj->description)) $dataValid = false;
+		if(!isset($obj->additional_description)) $dataValid = false;
+		if(!isset($obj->price)) $dataValid = false;
+		if(!isset($obj->password)) $dataValid = false;
 
-			// Sende die Daten an die Datenbank
-			if($obj->operation == "add") {
-				sendToDatabase($pdo, $menu_item);
-			}
-			else {
-				echo "Unknown Request";
-				return;
-			}
+		return $dataValid;
+	}
 
+	/* ---------------------------------------------------------------
+	// Formatiert die übergebene Daten und sendet sie an die Datenbank
+	// --------------------------------------------------------------*/
+	function sendToDatabase($pdo, $menu_item) {
+		$statement = $pdo->prepare("INSERT INTO menuitem(RestaurantId, Day, Description, AdditionalDescription, Price) VALUES (:rid, :day, :descr, :add, :price)");
+		$statement->execute([
+			'rid' => $menu_item->restaurant->id,
+			'day' => $menu_item->day,
+			'descr' => $menu_item->descr,
+			'add' => $menu_item->add_descr,
+			'price' => $menu_item->price,
+		]);
+	}
 
-			/* ---------------------------------------------------------------
-			// Fehlerausgabe
-			// --------------------------------------------------------------*/
-			function errorMessage() {
-				echo "<b>Fehler!</b>";
-				return;
-			}
+	/* ---------------------------------------------------------------
+	// Erzeugt aus den übergebene Daten ein MenuItem-Objekt
+	// --------------------------------------------------------------*/
+	function createMenuItem($res, $data)
+	{
+		$menuItem = new MenuItem($data->day, $res, $data->description, $data->price, $data->additional_description, null, null);
+		return $menuItem;
+	}
 
-			/* ---------------------------------------------------------------
-			// Prüft die übergebene Daten, bevor diese in die Datenbank 
-			// geschrieben werden.
-			// --------------------------------------------------------------*/
-			function validateData($obj) {
-				$dataValid = true;
+	/* ---------------------------------------------------------------
+	// Bearbeitung POST Request
+	// "restaurant" erlaubt, nur die Daten eines bestimmten restaurants
+	//		zu zeigen. (Default: all)
+	// "mode" konfiguriert die Anzeige:
+	//		archive: Alle vorhandenen Daten zeigen
+	//		default: Diese Woche anzeigen
+	//		nosy: Diese Woche und nächste anzeigen
+	----------------------------------------------------------------*/
+	function get() {
+		$restaurant = $_POST['restaurant'];
+		$mode = $_POST['mode'];
 
-				// Prüfe ob alle notwendigen Daten vorhanden sind
-				if(!isset($obj->operation)) $dataValid = false;
-				if(!isset($obj->restaurant)) $dataValid = false;
-				if(!isset($obj->day)) $dataValid = false;
-				if(!isset($obj->description)) $dataValid = false;
-				if(!isset($obj->additional_description)) $dataValid = false;
-				if(!isset($obj->price)) $dataValid = false;
-				if(!isset($obj->password)) $dataValid = false;
+		//query formatieren
 
-				return $dataValid;
-			}
+		//absenden mit Read Only Account
 
-			/* ---------------------------------------------------------------
-			// Formatiert die übergebene Daten und sendet sie an die Datenbank
-			// --------------------------------------------------------------*/
-			function sendToDatabase($pdo, $menu_item) {
-				$statement = $pdo->prepare("INSERT INTO menuitem(RestaurantId, Day, Description, AdditionalDescription, Price) VALUES (:rid, :day, :descr, :add, :price)");
-				$statement->execute([
-					'rid' => $menu_item->restaurant->id,
-					'day' => $menu_item->day,
-					'descr' => $menu_item->descr,
-					'add' => $menu_item->add_descr,
-					'price' => $menu_item->price,
-				]);
-			}
+		//rückgabe (JSON)
 
-			/* ---------------------------------------------------------------
-			// Erzeugt aus den übergebene Daten ein MenuItem-Objekt
-			// --------------------------------------------------------------*/
-			function createMenuItem($res, $data)
-			{
-				$menuItem = new MenuItem($data->day, $res, $data->description, $data->price, $data->additional_description, null, null);
-				return $menuItem;
-			}
+		return;
+	}
 
-			?>
-	
-	</body>
-</html>
+	?>
